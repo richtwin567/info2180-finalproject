cd1d122fc91e2911c28940f778c7a25d22290dfc
Rojae Martin <rojaedmartin@gmail.com>
2020-12-04 02:39:59 -0500

    Added session.php functions and added form to index.html

--- server/session.php
+++ server/session.php
@@ -1,15 +1,19 @@
 <?php
-function loginUser($con) {
+
+require_once('database.php');
+
+function loginUser($conn) {
     session_start();
     if(isset($_POST['Login'])) {
        if(empty($_POST['email']) || empty($_POST['password'])) {
             echo 'Please enter your email/password';
        } else {
             $query = "select * from user where email='" . $_POST['email'] . "' and password='". $_POST['password'] . "'";
-            $result = mysqli_query($con, $query);
+            $result = mysqli_query($conn, $query);
 
             if(mysqli_fetch_assoc($result)) {
                 $_SESSION['email']=$_POST['email'];
+                isLoggedIn(true);
                 //Link to dashboard
             } else {
                 echo "Invalid email/password";
@@ -20,11 +24,15 @@ function loginUser($con) {
     }
 }
 
-function logoutUser()
-{
+function logoutUser() {
+    if(isset($_GET['logout'])) {
+        session_destroy();
+        isLoggedIn(false);
+        //link to Login screen
+    }
 }
 
-function isLoggedIn()
-{
+function isLoggedIn($loggedIn) {
+    return $loggedIn;
 }
 ?>
