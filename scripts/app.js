8f9704deb0d451e2499ee48d074588ef5fefb2cf
DangaRanga <jasonblue1626@gmail.com>
2020-12-03 23:51:39 -0500

    Removed non functional js

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,4 @@
-var registrationArea = docment.getElementById('register-user');
-var issueCreationArea = document.getElementById('issue-creation');
-var homeArea = document.getElementById('issues');
-
 document.addEventListener('load', function() {
     console.log('We are in operation guys');
 
 });
-
-function toggleScreen(event) {
-    if (event.target && event.target.nodeName == "LI") {
-        let selectedElement = e.target.id;
-        if (selectedElement === 'home') {
-
-        } else if (selectedElement === 'add-user') {
-
-        } else if (selectedElement === 'add-issue') {
-
-        } else if (selectedElement === 'logout') {
-
-        }
-    }
-}
-
-function toggleVisibility(targetElement) {
-    if (targetElement.classList.contains('hidden')) {
-        targetElement.classList.remove('hidden');
-    } else {
-        targetElement.classList.add('hidden');
-    }
-}
