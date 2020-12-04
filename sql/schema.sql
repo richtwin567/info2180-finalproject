fef066c62f9b5c79779485bbb0630e1a56ea47eb
Rojae Martin <rojaedmartin@gmail.com>
2020-12-04 00:45:18 -0500

    Removed constraint on 'issue' table so that 'user' table could be created

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS `issuetracker` DEFAULT CHARACTER SET utf8mb4 COLLA
 USE `issuetracker`;
 DROP TABLE IF EXISTS `issues`;
 CREATE TABLE `issues`(
-    `id` int NOT NULL,
+    `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `title` varchar(225) COLLATE utf8mb4_general_ci NOT NULL,
     `description` text COLLATE utf8mb4_general_ci NOT NULL,
     `type` varchar(225) COLLATE utf8mb4_general_ci NOT NULL,
@@ -13,28 +13,17 @@ CREATE TABLE `issues`(
     `created` datetime NOT NULL,
     `updated` datetime NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
-ALTER TABLE `issues`
-MODIFY `id` int NOT NULL AUTO_INCREMENT;
-ALTER TABLE `issues`
-ADD PRIMARY KEY (`id`),
-    ADD KEY `created_by_fk` (`created_by`),
-    ADD KEY `assigned_to_fk` (`assigned_to`);
-ALTER TABLE `issues`
-ADD CONSTRAINT `assigned_to_fk` FOREIGN KEY (`assigned_to`) REFERENCES `users` (`id`) ON DELETE
-SET NULL ON UPDATE CASCADE,
-    ADD CONSTRAINT `created_by_fk` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
+
+
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-    `id` int NOT NULL,
+    `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `firstname` varchar(225) COLLATE utf8mb4_general_ci NOT NULL,
     `lastname` varchar(225) COLLATE utf8mb4_general_ci NOT NULL,
     `password` varchar(225) COLLATE utf8mb4_general_ci NOT NULL,
     `email` varchar(225) COLLATE utf8mb4_general_ci NOT NULL,
     `date_joined` datetime NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
-ALTER TABLE `users`
-MODIFY `id` int NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT;
+
 CREATE USER 'issuetrackeradmin' @'localhost' IDENTIFIED BY 'issueTRACKER11';
 GRANT ALL PRIVILEGES ON issuetracker.* TO 'issuetrackeradmin' @'localhost';
