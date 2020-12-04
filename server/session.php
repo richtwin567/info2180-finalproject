513c50ed162239c91a565903741cd6fcfcbc00f5
Rojae Martin <rojaedmartin@gmail.com>
2020-12-04 02:02:18 -0500

    Added to the loginUser function

--- server/session.php
+++ server/session.php
@@ -1,6 +1,23 @@
 <?php
-function loginUser()
-{
+function loginUser($con) {
+    session_start();
+    if(isset($_POST['Login'])) {
+       if(empty($_POST['email']) || empty($_POST['password'])) {
+            echo 'Please enter your email/password';
+       } else {
+            $query = "select * from user where email='" . $_POST['email'] . "' and password='". $_POST['password'] . "'";
+            $result = mysqli_query($con, $query);
+
+            if(mysqli_fetch_assoc($result)) {
+                $_SESSION['email']=$_POST['email'];
+                //Link to dashboard
+            } else {
+                echo "Invalid email/password";
+            }
+       }
+    } else {
+        echo 'Error in connecting';
+    }
 }
 
 function logoutUser()
