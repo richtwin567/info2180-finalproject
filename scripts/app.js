34e4d3343c6d2bbd22b780bc97b83c8b8e26f602
DangaRanga <jasonblue1626@gmail.com>
2020-12-04 18:03:21 -0500

    Added sidebar icons and basic form validation

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,7 @@
-document.addEventListener('load', function() {
-    console.log('We are in operation guys');
-
+window.addEventListener('load', function() {
+    console.log('test');
+    let passwordField = document.getElementById('password');
+    let passwordStr = `Invalid password: Must contain at least one number and 
+      one uppercase and lowercase letter, and at least 8 or more characters`
+    passwordField.setCustomValidity(passwordStr);
 });
