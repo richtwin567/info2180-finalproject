2b70beaa22b1197b52b96d046d7aedf606b745dd
DangaRanga <jasonblue1626@gmail.com>
2020-12-05 13:47:50 -0500

    Added basic code for registering

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 // The main driver for the application. All function calls are made to this module. 
 import { serveContent } from './display.js';
+import { registerUser } from './auth.js';
 
 window.addEventListener('load', function() {
     serveContent();
+    let registrationBtn = document.querySelector('#register-user .btn');
+    registrationBtn.addEventListener('click', registerUser(e));
 });
--- scripts/auth.js
+++ scripts/auth.js
@@ -1,1 +1,14 @@
 //  This module handles the authentication of users in the application
+
+function authenticateUser() {
+    let submitBtn = document.getElementById('')
+}
+
+function registerUser(e) {
+    e.preventDefault();
+    let formElements = document.getElementById('register-user').elements;
+    console.log(formElements);
+}
+
+
+export { registerUser }
