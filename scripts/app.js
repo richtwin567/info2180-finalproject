312432110e62d1dbef770265c24a23036b9faabc
omisoul <marlonjames24@gmail.com>
2020-12-05 14:16:00 -0500

    Fixed sidebar

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-import { fetchissues } from './issues.js';
-
-document.addEventListener('load', function() {
-    console.log('We are in operation guys');
-    const homeBtn = document.querySelector('#home');
-    const addUserBtn = document.querySelector('#add-user');
-    const addIssueBtn = document.querySelector('#add-issue');
-    const logoutBtn = document.querySelector('#logout')
-});
-=======
 // The main driver for the application. All function calls are made to this module. 
 import { serveContent } from './display.js';
 
 window.addEventListener('load', function() {
     serveContent();
 });
->>>>>>> 2556bc05af3d2d35225617b6ec551d21b40cee8c
