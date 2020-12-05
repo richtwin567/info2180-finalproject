b4bb52876d2f2995a3fadae00b84daeb1ebcebb1
Rojae Martin <rojaedmartin@gmail.com>
2020-12-04 23:24:24 -0500

    Added session functions

--- server/session.php
+++ server/session.php
@@ -1,13 +1,26 @@
 <?php
-function loginUser()
-{
+function loginUser($conn, $email) {
+    $query = "SELECT * FROM users WHERE email = '$email'";
+    $_SESSION['userInfo'] = mysqli_query($conn,$query);
+
+    $_SESSION['id'] = $_SESSION['userInfo'][0];
+    $_SESSION['firstname'] = $_SESSION['userInfo'][1];
+    $_SESSION['lastname'] = $_SESSION['userInfo'][2];
+    $_SESSION['password'] = $_SESSION['userInfo'][3];
+    $_SESSION['email'] = $_SESSION['userInfo'][4];
+    $_SESSION['date_joined'] = $_SESSION['userInfo'][5];
+
+    $_SESSION['user'] = $_SESSION['firstname'] . ' ' . $_SESSION['lastname'];
 }
 
-function logoutUser()
-{
+function logoutUser() {
+    session_destroy();
+    header("location:index.html?=login"); //redirect to login screen
 }
 
-function isLoggedIn()
-{
+function isLoggedIn() {
+    if(isset($_SESSION['user'])) {
+        return true;
+    }
 }
 ?>
