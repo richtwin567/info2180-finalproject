63d58be386544f8b5eafad4f130591776e661be7
omisoul <marlonjames24@gmail.com>
2020-12-05 13:59:14 -0500

    Initial JS

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,8 @@ import { fetchissues } from './issues.js';
 
 document.addEventListener('load', function() {
     console.log('We are in operation guys');
-
+    const homeBtn = document.querySelector('#home');
+    const addUserBtn = document.querySelector('#add-user');
+    const addIssueBtn = document.querySelector('#add-issue');
+    const logoutBtn = document.querySelector('#logout')
 });
--- scripts/issues.js
+++ scripts/issues.js
@@ -1,14 +1,20 @@
 export async function fetchissues(){
     let url = 'issues.php/issues'
-    const response = await fetch(url);
-    if(response.ok){
-        // Returns the response as a string
-        console.log (response);
-        return response.text();
-    
-    // If any unexpected errors happen while fetching, an error is thrown
-    }else{
-        const message = `An error has occured: ${response.status}`;
-        throw new Error(message);
+    // Try catch implementation 
+    try{
+        const response = await fetch(url);
+
+    }catch(error){
+        console.log(error.message)
     }
+    // if(response.ok){
+    //     // Returns the response as a string
+    //     console.log (response);
+    //     return response.text();
+    
+    // // If any unexpected errors happen while fetching, an error is thrown
+    // }else{
+    //     const message = `An error has occured: ${response.status}`;
+    //     throw new Error(message);
+    // }
 }  
